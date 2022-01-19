2626fd9a93a87694ce67c2a759e6553c65979856
kosaj <kosaj@users.noreply.github.com>
2022-01-20 00:27:47 +0100

    .

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,24 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './feature/home/home.component';
+import { RootComponent } from './feature/root/root.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: RootComponent,
+    children: [
+      { path: '', component: HomeComponent },
+      // { path: '' },
+      // { path: '' },
+      // { path: '' },
+      // { path: '' },
+    ],
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { RootComponent } from './feature/root/root.component';
+import { HomeComponent } from './feature/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RootComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
